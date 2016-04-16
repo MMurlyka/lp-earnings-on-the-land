7332ab63f84a5b32f2430cbeab0b2fcd96f81719
Milok Murlyka <milok.murlika@yandex.ru>
2016-04-17 02:57:36 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(window).load(function() {
+	new WOW().init();
+});
+
+$(window).ready(function() {
+
+	/* Timer  Start */
+	if($.cookie('timer')) {
+		$timer = new Date(+$.cookie('timer'));
+
+		if($timer < new Date()) {
+			$.cookie('timer', null);
+		}
+		
+	} 
+
+	if(!$.cookie('timer') || $.cookie('timer') == "null") {
+		$date = new Date();
+		$timer = new Date($date.setMinutes($date.getMinutes() + 30));
+		$.cookie('timer', $timer.getTime());
+	}
+
+	$('#clock').countdown($timer, function(event) {
+		$(this).html(event.strftime('<span class="circle"><span class="nn">%M</span></span> : <span class="circle"><span class="nn">%S</span></span>'));
+	});
+
+	/* Timer End*/
+	/* Slick slider Start*/
+		$(".slick").slick({
+			slidesToShow: 1,
+			arrows: true,
+			dots: true
+		});
+	/* Slick slider End*/
+	/* Form star */
+	$("form").submit(function() {
+		var $email = $(this).find(".in-email"),
+			reg = /.+@.+\..+/i;
+
+		if(!~$email.val().search(reg)) {
+			$email.addClass("invalid");
+
+			return false;
+		} 
+
+		yaCounter36798385.reachGoal('form');
+		$email.removeClass("invalid");
+			
+
+	});
+	/* Form end */
+	/* Popup start */
+	$(".link-mgf").magnificPopup()
+	/* Popup end */
+
+});
+
